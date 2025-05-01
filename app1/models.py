9d3a3d1c46644016221da5e395896e82cb1a2bbf
priyanka carpenter <priyankacarpenter30@gmail.com>
2025-05-01 23:53:08 +0530

    Created question, answer, result model

--- app1/models.py
+++ app1/models.py
@@ -10,3 +10,26 @@ class Student(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class Question(models.Model):
+    subject = models.CharField(max_length=50)
+    text = models.TextField()
+    option_a = models.CharField(max_length=255)
+    option_b = models.CharField(max_length=255)
+    option_c = models.CharField(max_length=255)
+    option_d = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=1)  # 'A', 'B', 'C', or 'D'
+
+    def __str__(self):
+        return f"{self.subject}: {self.text[:50]}"
+
+class Answer(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    selected_answer = models.CharField(max_length=1, blank=True, null=True)  # A, B, C, D
+
+class Result(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
